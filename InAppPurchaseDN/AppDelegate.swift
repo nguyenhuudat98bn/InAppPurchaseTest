418617ecc234b28fe008c13f22fd46ab5e585d7d
Đạt Nguyễn hữu <datnguyendev.ios@gmail.com>
2022-03-11 00:05:35 +0700

    push

--- InAppPurchaseDN/AppDelegate.swift
+++ InAppPurchaseDN/AppDelegate.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import PushKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
+    
+    var voipRegistry: PKPushRegistry?
+    
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
+        let deviceTokenString = pushCredentials.token.map { String(format: "%02.2hhx", $0) }.joined()
+                   NSLog("deviceTokenString \(deviceTokenString)")
+
+    }
+    
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerForVoIPPushes()
         return true
     }
 
@@ -24,6 +35,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    func registerForVoIPPushes() {
+        let mainQueue = DispatchQueue.main
+        self.voipRegistry = PKPushRegistry.init(queue: mainQueue)
+        self.voipRegistry?.delegate = self
+        self.voipRegistry?.desiredPushTypes = [.voIP]
+    }
+
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
