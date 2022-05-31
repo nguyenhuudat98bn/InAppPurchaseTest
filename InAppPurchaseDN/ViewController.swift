00367ba4c95a72719f6a8a5921cf7cf17b488ab2
Đạt Nguyễn hữu <datnguyendev.ios@gmail.com>
2022-05-31 09:14:35 +0700

    push code

--- InAppPurchaseDN/ViewController.swift
+++ InAppPurchaseDN/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        PKIAPHandler.shared.setProductIds(ids: ["", ""])
+        PKIAPHandler.shared.setProductIds(ids: ["iapstg5000VND"])
         PKIAPHandler.shared.fetchAvailableProducts { [weak self](products) in
            guard let sSelf = self else {return}
            sSelf.productsArray = products
@@ -22,11 +22,21 @@ class ViewController: UIViewController {
         }
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func getReceipt(){
+      
+    }
 
     @IBAction func point1Touch(){
         PKIAPHandler.shared.purchase(product: self.productsArray[0]) { (alert, product, transaction) in
            if let tran = transaction, let prod = product {
                print("tran\(tran)prod\(prod)")
+               if let url = Bundle.main.appStoreReceiptURL,
+                  let data = try? Data(contentsOf: url) {
+                     let receiptBase64 = data.base64EncodedString()
+                   print("TTTTTT\(receiptBase64)")
+                     // Send to server
+               }
              //use transaction details and purchased product as you want
            }
            }
